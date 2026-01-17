45c4c6b569db02d689640e492994416a78a5f2bf
Michael <dxntlxxkbxck@gmail.com>
2026-01-17 03:09:31 +0300

    done

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-	// функция для показа виджета "сегодня"
-	showTodayWidget: () => ipcRenderer.invoke('show-today')
-});
